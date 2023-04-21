edb748883654e1b86a575467d029aca0db6bda53
Shi Lei <shi_lei@massclouds.com>
2023-04-21 09:46:26 +0800

    rust: add rust-src

--- helloworld/build.sh
+++ helloworld/build.sh
@@ -1,3 +1,9 @@
-cargo build -Zbuild-std=core,alloc,std,panic_abort \
+#!/bin/sh
+
+export XARGO_RUST_SRC=/home/cloud/gitRust/step-hermit/rust/library
+
+cargo xbuild -Zbuild-std=core,alloc,std,panic_abort \
     -Zbuild-std-features=compiler-builtins-mem \
-    --target x86_64-unknown-hermit
+    --target x86_64-unknown-monk.json
+
+    #--target x86_64-unknown-hermit
