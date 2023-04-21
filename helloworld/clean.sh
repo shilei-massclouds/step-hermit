40042bbe3be29abfa73a5f11a54b0bbb07feca34
Shi Lei <shi_lei@massclouds.com>
2023-04-21 22:04:02 +0800

    maint: fix cleanup.sh

--- helloworld/clean.sh
+++ helloworld/clean.sh
@@ -2,6 +2,8 @@ cargo clean -Zbuild-std=core,alloc,std,panic_abort \
     -Zbuild-std-features=compiler-builtins-mem \
     --target x86_64-unknown-monk.json
 
+rm -rf ./rusty-loader/target
+
 rm -rf ../libhermit-rs/x86_64* ../libhermit-rs/debug
 
     #--target x86_64-unknown-hermit
